b7aa47582305bcf0b6f3af7504c3da4b35afecd6
jzx <jzx404@outlook.com>
2026-05-10 10:09:57 +0000

    新增GVN（全局值编号）优化pass
    实现基于值编号的公共子表达式消除，识别并合并等价计算
    
    (cherry picked from commit c2f09c6b928504f4dff6a29d4b8cf2bf11d386af)

---
+++ ir/passes/fixedPointFunctionPass/GVN.h
@@ -0,0 +1,23 @@
+///
+/// @file GVN.h
+/// @brief 基于支配树的全局值编号 pass
+///
+
+#pragma once
+
+class Function;
+class Module;
+
+class GVN {
+
+public:
+    GVN(Function * func, Module * mod);
+
+    /// @brief 对函数执行支配树 GVN
+    /// @return 若删除了冗余值计算则返回 true
+    bool run();
+
+private:
+    Function * func = nullptr;
+    Module * mod = nullptr;
+};
