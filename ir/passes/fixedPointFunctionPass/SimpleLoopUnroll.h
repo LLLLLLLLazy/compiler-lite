5b9c96e8c8a86c79ce7f4e93ac04cafed49d674d
jzx <jzx404@outlook.com>
2026-05-10 10:10:53 +0000

    新增简单循环展开优化pass（SimpleLoopUnroll）
    对小型循环进行展开以减少循环开销和暴露指令级并行
    
    (cherry picked from commit 98ae85531ce1c7f7dad056ee89a6698dbd915420)

---
+++ ir/passes/fixedPointFunctionPass/SimpleLoopUnroll.h
@@ -0,0 +1,26 @@
+///
+/// @file SimpleLoopUnroll.h
+/// @brief 小常数循环完全展开 pass。
+///
+
+#pragma once
+
+class Function;
+class Instruction;
+class Module;
+
+class SimpleLoopUnroll {
+
+public:
+    SimpleLoopUnroll(Function * func, Module * mod);
+
+    /// @brief 展开形如 for (i = const; i < const; ++i) 的小循环。
+    bool run();
+
+private:
+    bool tryUnrollHeader(class BasicBlock * header);
+    Instruction * cloneInstruction(Instruction * inst);
+
+    Function * func = nullptr;
+    Module * mod = nullptr;
+};
