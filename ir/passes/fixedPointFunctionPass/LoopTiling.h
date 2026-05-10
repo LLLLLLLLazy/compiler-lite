69dd469f22b8de2994238f3b66390291d3b940ed
jzx <jzx404@outlook.com>
2026-05-10 10:10:40 +0000

    新增循环分块优化pass（LoopTiling）
    对嵌套循环进行分块变换，提升缓存局部性
    
    (cherry picked from commit 0e90f0b969f8c14fe932d59f370ab114f5437cd0)
    (cherry picked from commit fe6e113261d08d84301662eb7122750fe2539389)

---
+++ ir/passes/fixedPointFunctionPass/LoopTiling.h
@@ -0,0 +1,29 @@
+///
+/// @file LoopTiling.h
+/// @brief 保守的二维循环分块 pass
+///
+
+#pragma once
+
+#include <cstdint>
+
+class BasicBlock;
+class Function;
+class Module;
+
+class LoopTiling {
+
+public:
+    LoopTiling(Function * func, Module * mod, int32_t tileSize = 32);
+
+    /// @brief 将安全的二维 unit-stride 循环改写为 32x32 分块循环。
+    /// @return 若修改了 IR 则返回 true
+    bool run();
+
+private:
+    bool tryTileHeader(BasicBlock * header, class LoopInfo & loopInfo);
+
+    Function * func = nullptr;
+    Module * mod = nullptr;
+    int32_t tileSize = 32;
+};
