58bf147ba1dfab8726f416aa85b8c041cfd2f046
jzx <jzx404@outlook.com>
2026-07-25 15:40:57 +0800

    fix: sylib计时全局变量定义移入sylib.c
    头文件中的变量定义改为声明式注释,避免多翻译单元重复定义

--- tests/sylib.c
+++ tests/sylib.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
 #include<stdarg.h>
 #include<sys/time.h>
-#include"sylib.h"
+
+struct timeval _sysy_start,_sysy_end;
+#define starttime() _sysy_starttime(__LINE__)
+#define stoptime()  _sysy_stoptime(__LINE__)
+#define _SYSY_N 1024
+int _sysy_l1[_SYSY_N],_sysy_l2[_SYSY_N];
+int _sysy_h[_SYSY_N], _sysy_m[_SYSY_N],_sysy_s[_SYSY_N],_sysy_us[_SYSY_N];
+int _sysy_idx;
+__attribute((constructor)) void before_main(); 
+__attribute((destructor)) void after_main();
+
 /* Input & output functions */
 int getint(){int t; scanf("%d",&t); return t; }
 int getch(){char c; scanf("%c",&c); return (int)c; }
