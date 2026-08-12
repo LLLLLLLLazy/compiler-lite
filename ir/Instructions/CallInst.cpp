d9763844d3c6eb03de3ba467853a64c0f8ba5603
杨茂靓 <1391078722@qq.com>
2026-08-12 19:22:49 +0800

    fix: 兼容旧版 LLVM 可变参数调用

--- ir/Instructions/CallInst.cpp
+++ ir/Instructions/CallInst.cpp
@@ -25,13 +25,30 @@ Value * CallInst::getArg(int32_t i)
     return getOperand(i);
 }
 
+/// @brief 将调用指令格式化为 LLVM IR 文本
+/// @param str 接收格式化结果的字符串
 void CallInst::toString(std::string & str)
 {
     bool hasResult = hasResultValue();
     if (hasResult) {
         str = getIRName() + " = ";
     }
-    str += "call " + getType()->toString() + " " + callee->getIRName() + "(";
+    str += "call " + getType()->toString() + " ";
+    if (callee->isVarArg()) {
+        str += "(";
+        const auto & params = callee->getParams();
+        for (std::size_t i = 0; i < params.size(); ++i) {
+            if (i > 0) {
+                str += ", ";
+            }
+            str += params[i]->getType()->toString();
+        }
+        if (!params.empty()) {
+            str += ", ";
+        }
+        str += "...) ";
+    }
+    str += callee->getIRName() + "(";
     for (int32_t i = 0; i < getArgCount(); ++i) {
         if (i > 0) {
             str += ", ";
